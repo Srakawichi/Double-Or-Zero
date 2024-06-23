4e84017793a84e08bffd70ed30a504f97debb8a4
Mihoshi <74931190+Srakawichi@users.noreply.github.com>
2024-06-23 21:29:09 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-let balance = 99000;
+let balance = 1000;
 let count = 1;
 
 function balanceIsZero(balance) {
@@ -34,34 +34,40 @@ function placeBet(bet) {
     document.getElementById('balance').textContent = balance;
     console.log("Runde " + count + " Kontostand: " + balance);
     count++;
-
-    if (balance <= 0) {
-        alert("Spiel vorbei");
+    backgroundImage()
+}
+function backgroundImage() {
+    if (balance === 0) {
         console.log("game over");
         disabledButton()
         document.getElementById('gameOverLabel').style.display = 'block';
+        document.body.style.backgroundImage = "url('images/homeless.jpeg')";
+        document.getElementById('LevelGod').style.display = 'none';
     }
     if (balance < 10000) {
         document.getElementById('Level2').style.display = 'none';
         document.body.style.backgroundColor = '#333';
+        document.body.style.backgroundImage = "url('images/worker.jpeg')";
     }
     if (balance >= 10000) {
         document.getElementById('Level2').style.display = 'block';
         document.body.style.backgroundColor = 'gray';
+        document.body.style.backgroundImage = "url('images/businessowner.jpeg')";
     }
     if (balance >= 50000) {
         document.getElementById('Level3').style.display = 'block';
         document.getElementById('Level2').style.display = 'none';
         document.getElementById('LevelGod').style.display = 'none';
         document.body.style.backgroundColor = '#228680';
+        document.body.style.backgroundImage = "url('images/Invester.jpeg')";
     }
-    if (balance >= 100000) {
+    if (balance >= 1000000) {
         document.getElementById('LevelGod').style.display = 'block';
         document.getElementById('Level3').style.display = 'none';
-        document.body.style.backgroundImage = "url('C:\Users\rebek\Programme\Python\Baccarat\images\gambelgod.jpeg')";
+        document.body.style.backgroundImage = "url('images/gambelgod.png')";
+        
     }
 }
-
 function getBet() {
      const entryValue = document.getElementById('bet').value;
      const intValue = parseInt(entryValue, 10);
